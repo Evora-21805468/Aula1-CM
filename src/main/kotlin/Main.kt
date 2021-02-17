dd24981aca4f76f936890d044dd21ac288e9bba2
Evora-21805468 <Gofuckyourself98>
2021-02-17 09:21:35 +0000

    tudo feito

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,31 @@
-fun main (args: Array<String>) {
-    var listaNums = listOf<Int>(1,2,3,4,5,6,7,8,9,10)
-    var listaNomes = listOf<String>("Gonçalo","Bia","Martim","Catarina","João")
-    println(listaNums)
+fun main(args: Array<String>) {
+    var listaNums = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    var listaNomes = listOf<String?>("Gonçalo", "Bia", "Martim", "Catarina", "João")
 
-    for (i in listaNums){
-        if(listaNums[i-1] %2 != 0){
-            println(listaNums[i-1])
+    println(listaNums)
+    listaNomes += null
+    for (i in listaNums) {
+        if (listaNums[i - 1] % 2 != 0) {
+            println(listaNums[i - 1])
         }
     }
 
-    for (j in listaNomes){
-        cumprimentar(listaNomes[j])
-    }
+    listaNomes += null
 
+    for ((index, value) in listaNomes.withIndex()) {
+        if (value != null) {
+            if (value.length > 4) {
+                println("$index: ${cumprimentar(value)}, tudo bem?")
+            } else {
+                println("$index: ${cumprimentar(null)}, tudo bem?")
+            }
+        }
+    }
 }
 
-fun cumprimentar (nomePessoa: String): String {
+fun cumprimentar(nomePessoa: String?): String {
+    if (nomePessoa == null) {
+        return "Olá Anónimo"
+    }
     return "Olá $nomePessoa"
 }
